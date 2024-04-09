d69d3bdbd12a6022b3722edaac5f0af55f92f3d7
Diego <diego.milla.26@gmail.com>
2024-04-09 08:53:18 +0200

    Re-enable React strict mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Setting to true somehow leads to a error on initial FeatureFlags load, leading to a temporary flash on page load
+  reactStrictMode: true, // This should not affect any implications in productions, and helps with debugging.
 };
 
 module.exports = nextConfig;
