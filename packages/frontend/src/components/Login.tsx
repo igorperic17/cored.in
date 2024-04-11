8e206621c7e865e21486df06c0934aa6268c9724
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-11 13:49:30 +0200

    Restyle the Connect and Disconnect buttons in Login

--- packages/frontend/src/components/Login.tsx
+++ packages/frontend/src/components/Login.tsx
@@ -1,26 +1,79 @@
 import { useClientContext } from "@/hooks/useClientContext";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, HStack, Text } from "@chakra-ui/react";
 
 export const Login = () => {
   const { connectWallet, disconnect, walletAddress } = useClientContext();
   const isConnected = walletAddress.length;
 
+  const shortWalletAddress =
+    walletAddress.slice(0, 3) + "..." + walletAddress.slice(-3);
+
   if (isConnected) {
     return (
-      <div className="main">
-        <div className="title">Connected</div>
-        <div>{walletAddress}</div>
-        <Button className="card" onClick={() => disconnect()}>
+      // V1
+      // <HStack>
+      //   <Text>{walletAddress}</Text>
+      //   <Button variant="primary" size="xs" onClick={() => disconnect()}>
+      //     Disconnect
+      //   </Button>
+      // </HStack>
+
+      // V2
+      // <HStack
+      //   bg="colors.background.900"
+      //   borderRadius="3xl"
+      //   px="0.25em"
+      //   py="0.25em"
+      //   pl="1em"
+      // >
+      //   <Text color="colors.brand.600">{walletAddress}</Text>
+      //   <Button
+      //     borderStartStartRadius="0"
+      //     borderEndStartRadius="0"
+      //     borderStartEndRadius="xl"
+      //     borderEndEndRadius="xl"
+      //     bg="colors.background.900"
+      //     color="colors.text.100"
+      //     size="xs"
+      //     onClick={() => disconnect()}
+      //   >
+      //     Disconnect
+      //   </Button>
+      // </HStack>
+
+      // V3
+      <HStack
+        bg="colors.background.900"
+        borderRadius="3xl"
+        px="0.25em"
+        py="0.25em"
+        pl="1em"
+      >
+        <Text color="colors.brand.600" textTransform="uppercase">
+          {shortWalletAddress}
+        </Text>
+        <Button
+          borderStartStartRadius="0"
+          borderEndStartRadius="0"
+          borderStartEndRadius="xl"
+          borderEndEndRadius="xl"
+          bg="none"
+          color="colors.text.100"
+          size="xs"
+          _hover={{
+            bg: "none"
+          }}
+          onClick={() => disconnect()}
+        >
           Disconnect
         </Button>
-        {/* <NetworkSwitcher /> */}
-      </div>
+      </HStack>
     );
   } else {
     return (
       <>
-        <Button className="card" onClick={connectWallet}>
-          {`Login`}
+        <Button variant="primary" size="md" onClick={connectWallet}>
+          Connect Wallet
         </Button>
       </>
     );
