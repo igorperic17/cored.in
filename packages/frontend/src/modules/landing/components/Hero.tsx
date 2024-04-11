d1c9f8fac11f50f30755fc4a4429f1c153f06389
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-11 14:48:18 +0200

    Add Hero component with its layout

---
+++ packages/frontend/src/modules/landing/components/Hero.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+
+export const Hero = () => {
+  return (
+    <Box w="100%" h="100vh">
+      <Flex
+        w="100%"
+        h="100%"
+        direction="column"
+        align="start"
+        justify="center"
+        gap="3em"
+      >
+        <Heading as="h2" fontSize="5rem" textAlign="left" maxW="750px">
+          This is a big heading in Hero section about the project
+        </Heading>
+        <Text
+          textAlign="right"
+          maxW="400px"
+          //   border="1px solid red"
+          alignSelf="end"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          voluptates tenetur illum ipsum cupiditate a, illo repellendus rerum
+          eligendi excepturi molestias laboriosam labore, non ad sequi.
+          Architecto fugiat adipisci velit!
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
--- packages/frontend/src/modules/landing/views/Landing.page.tsx
+++ packages/frontend/src/modules/landing/views/Landing.page.tsx
@@ -1,8 +1,13 @@
 import { Box, VStack } from "@chakra-ui/react";
+import { Hero } from "../components";
+
 const LandingPage = () => {
   return (
-    <Box mx="auto" maxW="1200px">
-      <VStack gap="11em" mb="16" px="2em"></VStack>
+    <Box mx="auto" maxW="1680px">
+      {/* <VStack gap="11em" mb="16" px="2em"> */}
+      <VStack px="2em">
+        <Hero />
+      </VStack>
     </Box>
   );
 };
