a1723828d3c78fd8378a1d63dd82374845e37f2b
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-16 16:05:18 +0200

    Edit Hero text and min-height

--- packages/frontend/src/modules/landing/components/Hero.tsx
+++ packages/frontend/src/modules/landing/components/Hero.tsx
@@ -2,8 +2,9 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const Hero = () => {
   return (
-    <Box w="100%" mt="6vh" h="min-content">
+    <Box w="100%" mt="6vh">
       <Flex
+        minH="80vh"
         w="100%"
         direction="column"
         align="start"
@@ -13,15 +14,10 @@ export const Hero = () => {
         <Heading as="h2" fontSize="5rem" textAlign="left" maxW="750px">
           Professional network with verified user information only
         </Heading>
-        <Text
-          textAlign="right"
-          maxW="350px"
-          //   border="1px solid red"
-          alignSelf="end"
-        >
-          Connect to professionals without a doubt in their experience. Share
-          your complete profile and receive messages only from the users to whom
-          you give permission.
+        <Text textAlign="right" maxW="400px" alignSelf="end">
+          Empower your professional experience with CoredIn. Customize your
+          profile visibility and choose who can reach out to you. It's time to
+          connect with confidence and unlock new opportunities.
         </Text>
       </Flex>
     </Box>
