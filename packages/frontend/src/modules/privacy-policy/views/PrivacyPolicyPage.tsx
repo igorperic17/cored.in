bca958326a5dede4a4618e1732dccca36e292d48
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-16 17:48:33 +0200

    Add Privacy Policy page text placeholder

--- packages/frontend/src/modules/privacy-policy/views/PrivacyPolicyPage.tsx
+++ packages/frontend/src/modules/privacy-policy/views/PrivacyPolicyPage.tsx
@@ -1,9 +1,11 @@
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, VStack, Text } from "@chakra-ui/react";
 
 const PrivatePolicyPage = () => {
   return (
     <Box mx="auto" maxW="1200px" gap="32px">
-      <VStack gap="11em" mb="16" px="32px"></VStack>
+      <VStack gap="11em" mb="16" px="32px" color="white">
+        <Text>Privacy Policy page here</Text>
+      </VStack>
     </Box>
   );
 };
