acf98bb9874ed2015d451a2103e423776c93d831
Ruben te Wierik <rtewwierik64@gmail.com>
2024-04-27 18:06:24 +0200

    Successful deployment of API gateway triggering Lambda - not tested yet.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -74,26 +74,51 @@ resource "aws_lambda_function" "lambda_backend" {
   }
 }
 
-resource "aws_apigatewayv2_api" "lambda_backend_api" {
-  name          = "${var.app_name}-backend-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "lambda_backend_api" {
+  name = "${var.app_name}-backend-api"
 }
 
-resource "aws_apigatewayv2_integration" "lambda_backend_main_integration" {
-  api_id             = aws_apigatewayv2_api.lambda_backend_api.id
-  integration_type   = "AWS_PROXY"
-  integration_method = "ANY"
-  integration_uri    = aws_lambda_function.lambda_backend.invoke_arn
+resource "aws_api_gateway_resource" "lambda_backend_api_proxy_resource" {
+  parent_id   = aws_api_gateway_rest_api.lambda_backend_api.root_resource_id
+  path_part   = "{proxy+}"
+  rest_api_id = aws_api_gateway_rest_api.lambda_backend_api.id
 }
 
-resource "aws_apigatewayv2_route" "root_route" {
-  api_id    = aws_apigatewayv2_api.lambda_backend_api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_backend_main_integration.id}"
+resource "aws_api_gateway_method" "lambda_backend_api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_backend_api.id
+  resource_id   = aws_api_gateway_resource.lambda_backend_api_proxy_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_route" "proxy_route" {
-  api_id    = aws_apigatewayv2_api.lambda_backend_api.id
-  route_key = "{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_backend_main_integration.id}"
+resource "aws_api_gateway_integration" "lambda_backend_api_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda_backend_api.id
+  resource_id             = aws_api_gateway_resource.lambda_backend_api_proxy_resource.id
+  http_method             = aws_api_gateway_method.lambda_backend_api_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_backend.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "lambda_backend_api_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_backend_api.id
+  resource_id = aws_api_gateway_resource.lambda_backend_api_proxy_resource.id
+  http_method = aws_api_gateway_method.lambda_backend_api_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "example_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_backend_api.id
+  resource_id = aws_api_gateway_resource.lambda_backend_api_proxy_resource.id
+  http_method = aws_api_gateway_method.lambda_backend_api_method.http_method
+  status_code = aws_api_gateway_method_response.lambda_backend_api_method_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_deployment" "lambda_backend_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_backend_api.id
+  stage_name  = "prod"
 }
