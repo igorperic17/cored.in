30b5883964389697648ed3ca7ff6484494f4668f
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-27 13:09:46 +0200

    Add social links to the footer

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,108 +1,48 @@
-import {
-  Flex,
-  Text,
-  Link,
-  Container,
-  VStack,
-  Heading,
-  HStack,
-  useMediaQuery
-} from "@chakra-ui/react";
-import { FaEnvelope, FaHeart } from "react-icons/fa6";
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Flex, Heading, Icon, Link, Text } from "@chakra-ui/react";
+import { FaXTwitter, FaDiscord } from "react-icons/fa6";
 
 export const Footer = () => {
-  const [isLargerThanLG] = useMediaQuery("(min-width: 62em)");
-
   return (
-    <Container
+    <Flex
       as="footer"
-      maxW="100vw"
-      centerContent
-      // bg="blackAlpha.50"
-      borderTop="1px solid black"
-      color="headingBlack"
+      flexDirection="column"
+      justify="center"
+      align="center"
+      w="100%"
+      h={{ base: "92vh", md: "90vh", xl: "50vh" }}
+      gap="3em"
     >
-      <Flex
-        // border="1px solid red"
-        direction={isLargerThanLG ? "row" : "column"}
-        gap={isLargerThanLG ? "" : "3em"}
-        justify="space-between"
-        align={isLargerThanLG ? "center" : "center"}
+      <Heading
+        as="h2"
+        fontSize={{ base: "3rem", md: "4rem" }}
+        mb="2rem"
         textAlign="center"
-        maxW="1200px"
-        w="100%"
-        px="6"
-        py="10"
+        maxW="600px"
       >
-        <VStack>
-          <Heading
-            as="h4"
-            fontSize="md"
-            w="100%"
-            color="headingBlack"
-            textAlign={isLargerThanLG ? "left" : "center"}
-            translate="no"
-          >
-            YOUR AMAZING COMPANY
-          </Heading>
-          <Text
-            textAlign={isLargerThanLG ? "left" : "center"}
-            whiteSpace="pre-line"
-            color="headingGrey"
-            translate="no"
-          >
-            {`C/ Some Road 1234
-            01234 City
-            Country`}
-          </Text>
-        </VStack>
-
-        {/* <Text mb="3">
-          Provided by{" "}
-          <Link href="https://cryptonize.world" isExternal color="blue.500">
-            Cryptonize.world
-          </Link>
-        </Text> */}
-        <Text
-          fontSize="md"
-          whiteSpace="pre-line"
-          justifySelf="center"
-          color="headingGrey"
-          order={isLargerThanLG ? "" : "1"}
-          ml={isLargerThanLG ? "2em" : "0"}
-          translate="no"
-          lineHeight="32px"
-        >
-          {`© Copyright Amazing Company
-          All Rights Reserved
-          Made with`}{" "}
-          <FaHeart style={{ display: "inline", color: "red" }} />
-        </Text>
-
-        <VStack>
-          <HStack spacing="1em">
-            <Link
-              as={ReactRouterLink}
-              to="mailto:info@yourcompany.world"
-              isExternal
-            >
-              <FaEnvelope size="26px" color="headingBlack" />
-            </Link>
-          </HStack>
-          <Text color="headingGrey" translate="no">
-            info@yourcompany.world
-          </Text>
-          <Link
-            as={ReactRouterLink}
-            to="/privacy-policy"
-            color="headingGrey"
-            isExternal
-          >
-            Privacy Policy
-          </Link>
-        </VStack>
+        Know first when we finish building cored
+        <Text as="span" color="brand.600">
+          .in
+        </Text>{" "}
+        for you
+      </Heading>
+      <Flex direction="row" justify="space-between" w="100%" maxW="180px">
+        <Link href="https://twitter.com/cored_in" isExternal fontSize="3rem">
+          <Icon
+            as={FaXTwitter}
+            _hover={{
+              color: "brand.600"
+            }}
+          />
+        </Link>
+        <Link href="https://discord.gg/wMc5uCkr" isExternal fontSize="3rem">
+          <Icon
+            as={FaDiscord}
+            _hover={{
+              color: "brand.600"
+            }}
+          />
+        </Link>
       </Flex>
-    </Container>
+    </Flex>
   );
 };
