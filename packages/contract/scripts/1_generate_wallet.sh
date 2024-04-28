897cc12de2ea721547fe26596a681cd7871721bf
Igor Peric <igor@biomech.us>
2024-04-28 15:31:14 +0200

    Contract scripts updates

--- packages/contract/scripts/1_generate_wallet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# generates a new wallet called "my_local_dev_wallet" on $COREUM_CHAIN_ID (should be coreum-testnet-1 for dev purposes)
-
-# cored keys add <name-of-the-key> --chain-id=$COREUM_CHAIN_ID
-cored keys add my_local_dev_wallet $COREUM_CHAIN_ID_ARGS
--- packages/contract/scripts/2_deploy.sh
+++ packages/contract/scripts/2_deploy.sh
@@ -2,7 +2,8 @@
 
 # deploys the already built contract to the $COREUM_CHAIN_ID_ARGS (should be testnet-1 for developement)
 
-# assumes a funded wallet called "my_local_dev_wallet" has been imported to the CLI
+# assumes a funded wallet called "my_local_dev_wallet" has been imported to the CLI using:
+# cored keys add my_local_dev_wallet --recover --chain-id=$CHAIN_ID
 
 RES=$(cored tx wasm store artifacts/coredin.wasm \
     --from my_local_dev_wallet --gas auto --gas-adjustment 1.3 -y -b block --output json $COREUM_NODE_ARGS $COREUM_CHAIN_ID_ARGS)
