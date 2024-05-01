96b09f42a887bcf60a28d7aa4f576939a1b5e5cb
Diego <diego.milla.26@gmail.com>
2024-05-01 15:54:10 +0200

    Center navbar on large screens

--- packages/frontend/src/components/Nav.tsx
+++ packages/frontend/src/components/Nav.tsx
@@ -7,7 +7,8 @@ import {
   Link,
   HStack,
   useMediaQuery,
-  useTheme
+  useTheme,
+  Box
 } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import { ROUTES } from "@/router/routes";
@@ -30,67 +31,75 @@ export const Nav: FC<NavProps> = ({ onOpen }) => {
   );
 
   return (
-    <Flex
+    <Box
       as="header"
-      direction="row"
-      justify="space-between"
-      align="center"
-      position="fixed"
+      position="sticky"
       top="0"
-      h={{ base: "8vh", md: "10vh", xl: "11vh" }}
-      w="100%"
-      maxW="1920px"
-      mx="auto"
-      px={{ base: "1.5em", md: "2.5em", lg: "3.5em", xl: "4em" }}
-      background="background.900"
-      // background="red"
       zIndex="10"
+      id="nav"
+      w="100%"
+      bg="background.900"
     >
-      <Heading as="h1" fontSize={{ base: "2rem", md: "3rem" }}>
-        cored
-        {/* <Text as="span" color="brand.500">
-          .in
-        </Text> */}
-      </Heading>
-      {isLargerThanMd && (
-        <HStack>
-          {navSections.map((item, index) => {
-            return (
-              <Link
-                key={`menu-section-item-${index}`}
-                as={ReactRouterLink}
-                to={item.link}
-                textDecoration="none"
-                textTransform="uppercase"
-                color={currentSection === item.title ? "brand.500" : ""}
-                fontSize="1.2em"
-                // fontFamily="heading"
-                _hover={{ color: "brand.500" }}
-                //_focus={{ color: "text.500" }}
-                _active={{ color: "" }}
-              >
-                {item.title}
-              </Link>
-            );
-          })}
-        </HStack>
-      )}
-      {isInitialised && isFeatureEnabled(FEATURE_FLAG.APP) && (
-        <Link as={ReactRouterLink} to={ROUTES.APP.path}>
-          <Button variant="primary" size="md">
-            Sign In
-          </Button>
-        </Link>
-      )}
-      {isInitialised && !isFeatureEnabled(FEATURE_FLAG.APP) && (
-        <Link as={ReactRouterLink} to={"#benefits"}>
-          <Button variant="primary" size="md">
-            Learn more
-          </Button>
-        </Link>
-      )}
-      {/* Placeholder while not initalized to avoid section links going to right */}
-      {!isInitialised && <Link as={ReactRouterLink} to={"#"}></Link>}
-    </Flex>
+      <Flex
+        direction="row"
+        justify="space-between"
+        align="center"
+        top="0"
+        h={{ base: "8vh", md: "10vh", xl: "11vh" }}
+        w="100%"
+        maxW="1920px"
+        mx="auto"
+        px={{ base: "1.5em", md: "2.5em", lg: "3.5em", xl: "4em" }}
+        background="background.900"
+        // background="red"
+        zIndex="10"
+      >
+        <Heading as="h1" fontSize={{ base: "2rem", md: "3rem" }}>
+          cored
+          <Text as="span" color="brand.500">
+            .in
+          </Text>
+        </Heading>
+        {isLargerThanMd && (
+          <HStack>
+            {navSections.map((item, index) => {
+              return (
+                <Link
+                  key={`menu-section-item-${index}`}
+                  as={ReactRouterLink}
+                  to={item.link}
+                  textDecoration="none"
+                  textTransform="uppercase"
+                  color={currentSection === item.title ? "brand.500" : ""}
+                  fontSize="1.2em"
+                  // fontFamily="heading"
+                  _hover={{ color: "brand.500" }}
+                  //_focus={{ color: "text.500" }}
+                  _active={{ color: "" }}
+                >
+                  {item.title}
+                </Link>
+              );
+            })}
+          </HStack>
+        )}
+        {isInitialised && isFeatureEnabled(FEATURE_FLAG.APP) && (
+          <Link as={ReactRouterLink} to={ROUTES.APP.path}>
+            <Button variant="primary" size="md">
+              Sign In
+            </Button>
+          </Link>
+        )}
+        {isInitialised && !isFeatureEnabled(FEATURE_FLAG.APP) && (
+          <Link as={ReactRouterLink} to={"#benefits"}>
+            <Button variant="primary" size="md">
+              Learn more
+            </Button>
+          </Link>
+        )}
+        {/* Placeholder while not initalized to avoid section links going to right */}
+        {!isInitialised && <Link as={ReactRouterLink} to={"#"}></Link>}
+      </Flex>
+    </Box>
   );
 };
