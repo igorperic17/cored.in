c3a312772c7a122d39807352db520342cdfbb694
Ruben te Wierik <rtewwierik64@gmail.com>
2024-05-02 10:43:08 +0200

    Remove details about website S3 bucket.

--- packages/frontend/deploy.sh
+++ packages/frontend/deploy.sh
@@ -2,13 +2,11 @@
 yarn build
 
 OUT_DIR=dist
-BUCKET=coredin-frontend-x3gt792u8mr5pj5y
-DISTRIBUTION_ID=EG97V09HP3548
 
-aws s3 sync $OUT_DIR s3://$BUCKET/ \
+aws s3 sync $OUT_DIR s3://$COREDIN_FRONTEND_BUCKET/ \
   --delete \
   --metadata-directive REPLACE \
   --cache-control max-age=31536000,public
 
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+# aws cloudfront create-invalidation --distribution-id $COREDIN_DISTRIBUTION_ID --paths "/*"
