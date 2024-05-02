f5c01b0ce34f210e002e81fee3875e84cdefb690
Ruben te Wierik <rtewwierik64@gmail.com>
2024-05-02 10:34:07 +0200

    Update frontend deployment script.

--- packages/frontend/deploy.sh
+++ packages/frontend/deploy.sh
@@ -2,14 +2,13 @@
 yarn build
 
 OUT_DIR=dist
-BUCKET=coredin-frontend
+BUCKET=coredin-frontend-x3gt792u8mr5pj5y
 DISTRIBUTION_ID=EG97V09HP3548
 
 aws s3 sync $OUT_DIR s3://$BUCKET/ \
   --delete \
   --metadata-directive REPLACE \
-  --cache-control max-age=31536000,public \
-  --acl public-read
+  --cache-control max-age=31536000,public
 
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
