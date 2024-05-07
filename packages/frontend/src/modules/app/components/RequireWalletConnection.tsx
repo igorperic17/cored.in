2990834977ad11a8d9be1255a9463cae594bb0fa
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-07 16:52:59 +0200

    Add a RequireWalletConnection component

---
+++ packages/frontend/src/modules/app/components/RequireWalletConnection.tsx
@@ -0,0 +1,54 @@
+import { Login } from "@/components";
+import {
+  VStack,
+  Text,
+  Heading,
+  Link,
+  Flex,
+  SimpleGrid
+} from "@chakra-ui/react";
+
+export const RequireWalletConnection = () => {
+  return (
+    <VStack
+      h="85vh"
+      align="center"
+      justify="center"
+      spacing="15vh"
+      textAlign="center"
+      maxW="750px"
+      mx="auto"
+      //   border="1px solid red"
+    >
+      <Heading as="h1" fontSize={{ base: "3rem", md: "4rem" }} mt="auto">
+        Connect your Keplr wallet to begin
+      </Heading>
+      <Login variant="primary" signInText="Connect wallet" />
+      <Text color="text.100" fontSize={{ base: "1rem", md: "" }} mt="-10vh">
+        Don't have a Keplr wallet?{" "}
+        <Link
+          href="https://www.keplr.app/"
+          isExternal
+          textDecoration="underline"
+          _hover={{
+            color: "brand.500"
+          }}
+          _focus={{
+            color: "brand.500"
+          }}
+        >
+          Get one here
+        </Link>
+      </Text>
+      <Text
+        color="text.600"
+        fontSize={{ base: "0.875rem", md: "1rem" }}
+        mt="auto"
+        // alignSelf="end"
+      >
+        All the information that you provide on cored.in can later be edited and
+        deleted by you.
+      </Text>
+    </VStack>
+  );
+};
