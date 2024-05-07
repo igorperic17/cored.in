fbc7af14292e01f0a69292c70dd4cc065814a2c7
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-07 14:30:29 +0200

    Render the Profile in the App.page

--- packages/frontend/src/modules/app/views/App.page.tsx
+++ packages/frontend/src/modules/app/views/App.page.tsx
@@ -1,24 +1,15 @@
-import { Login } from "@/components";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Profile } from "../components";
-import { Logo } from "@/components/Logo";
 
 const AppPage = () => {
   return (
-    <Box mx="auto" maxW="1680px">
-      <Flex
-        direction="column"
-        justify="center"
-        align="center"
-        h={{ base: "92vh", xl: "88vh" }}
-        px="2em"
-        gap="5em"
-        mb="16"
-      >
-        <Logo fontSize={{ base: "2rem", md: "3rem" }} />
-        <Login />
-        <Profile />
-      </Flex>
+    <Box
+      mx="auto"
+      w="100%"
+      maxW="1920px"
+      px={{ base: "1.5em", md: "2.5em", lg: "3.5em", xl: "4em" }}
+    >
+      <Profile />
     </Box>
   );
 };
