67645fa893cb38cd759ffd83e769cd8edb76d441
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-07 18:38:24 +0200

    Edit Progress texts

--- packages/frontend/src/modules/landing/components/OurProgress.tsx
+++ packages/frontend/src/modules/landing/components/OurProgress.tsx
@@ -18,22 +18,22 @@ const steps = [
   {
     title: "Profile registration",
     description:
-      "By connecting their Coreum wallet, users can create a cored.in profile, generate a DID and register it on-chain."
+      "Users can create a cored.in profile and generate a Decentralized Identifier (DID) by connecting their Keplr wallet on the Coreum chain, which is then registered on-chain."
   },
   {
     title: "Subscription-based verifiable claims",
     description:
-      "Profiles are enriched with verifiable credentials, whose content can have a public, anonymized or private visibility. Hidden information and claim verification will only be disclosed to profile subscribers (optionally paid)."
+      "Profiles on cored.in are enriched with verifiable credentials, allowing users to choose between public, anonymized, or private visibility for their content. Hidden information and claim verification are only disclosed to profile subscribers, who may optionally pay for access."
   },
   {
     title: "Incentivized messaging",
     description:
-      "To avoid spam, cored.in users can require upfront payment in order to be contacted, thus ensuring the mutual interest of the conversation."
+      "To prevent spam, cored.in users can request upfront payment before being contacted, ensuring mutual interest in the conversation."
   },
   {
     title: "Talent hunting",
     description:
-      "Find your next dream job or the best team for your project. With cored.in, you can verify the cantidate and the employers background, ensuring a fully transparent and trustless recruitment processs"
+      "Find your next dream job or the best team for your project. With cored.in, you can verify the candidate and the employer's background, ensuring a fully transparent and trustworthy recruitment process."
   }
 ];
 
