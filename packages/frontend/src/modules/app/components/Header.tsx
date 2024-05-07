283414c00f2b76f968f08eeab15c523f68793d38
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-07 14:28:09 +0200

    Add Header to the App

---
+++ packages/frontend/src/modules/app/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Login } from "@/components";
+import { Logo } from "@/components/Logo";
+import { Flex } from "@chakra-ui/react";
+
+export const Header = () => {
+  return (
+    <Flex
+      as="header"
+      direction="row"
+      justify="space-between"
+      align="center"
+      // border="1px solid red"
+      w="100%"
+      maxW="1920px"
+      mx="auto"
+      px={{ base: "1em", md: "2.5em", lg: "3.5em", xl: "4em" }}
+      py={{ base: "0.5em", md: "0.25em" }}
+      background="background.900"
+    >
+      <Logo fontSize={{ base: "1.5rem", md: "2rem" }} />
+      <Login />
+    </Flex>
+  );
+};
