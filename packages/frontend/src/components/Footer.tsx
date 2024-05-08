051a9a0e903d582d5bad3c56520f066f2fb53b7a
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-08 10:12:50 +0200

    Fix the Footer styling

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,46 +1,51 @@
 // import { BackToTop } from "@/modules/landing/components/BackToTop";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { SocialMedia } from "./SocialMedia";
 import { motion } from "framer-motion";
 import { fadeInAnimation } from "./constants/animations";
 
 export const Footer = () => {
   return (
-    <Flex
+    <Box
       as={motion.footer}
-      flexDirection="column"
-      justify="center"
-      align="center"
       w="100%"
       h="min-content"
       minH="50vh"
-      gap="3em"
-      // border="1px solid red"
-      pt="5em"
-      pb="10em"
-      mt="7em"
       initial="initial"
       whileInView="animate"
       variants={fadeInAnimation}
       viewport={{ once: true, amount: 0.3 }}
+      // border="1px solid red"
     >
-      <Heading
-        as="h2"
-        fontSize={{ base: "3rem", md: "4rem" }}
-        mb="2rem"
-        textAlign="center"
-        maxW="600px"
+      <Flex
+        flexDirection="column"
+        justify="center"
+        align="center"
+        gap="3em"
+        pt="5em"
+        pb="10em"
+        px="1em"
+        mt="7em"
       >
-        Get notified when cored
-        <Text as="span" color="brand.500">
-          .in
-        </Text>{" "}
-        is ready
-      </Heading>
-      <SocialMedia />
-      <Text>
-        {`All rights reserved - cored.in - ${new Date().getFullYear()}`}
+        <Heading
+          as="h2"
+          fontSize={{ base: "3rem", md: "4rem" }}
+          mb="2rem"
+          textAlign="center"
+          maxW="600px"
+        >
+          Get notified when cored
+          <Text as="span" color="brand.500">
+            .in
+          </Text>{" "}
+          is ready
+        </Heading>
+        <SocialMedia />
+      </Flex>
+      <Text color="text.400" textAlign="center" p="1em">
+        All rights reserved — cored.in &copy;{" "}
+        <Text as="span">{new Date().getFullYear()}</Text>
       </Text>
-    </Flex>
+    </Box>
   );
 };
