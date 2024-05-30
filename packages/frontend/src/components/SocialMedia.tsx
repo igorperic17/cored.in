2187abcc99f1b57687233408d05c70ee7332b324
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-30 15:04:06 +0200

    Update the props of the SocialMedia component

--- packages/frontend/src/components/SocialMedia.tsx
+++ packages/frontend/src/components/SocialMedia.tsx
@@ -5,12 +5,14 @@ import { FaXTwitter, FaDiscord } from "react-icons/fa6";
 type SocialMediaSize = {
   size: string;
   gap: string;
+  color?: string;
 };
 
-export const SocialMedia: FC<SocialMediaSize> = ({ size, gap }) => (
+export const SocialMedia: FC<SocialMediaSize> = ({ size, gap, color }) => (
   <Flex direction="row" justify="center" gap={gap}>
     <Link href="https://twitter.com/cored_in" isExternal fontSize={size}>
       <Icon
+        color={color}
         as={FaXTwitter}
         aria-label="X."
         _hover={{
@@ -20,6 +22,7 @@ export const SocialMedia: FC<SocialMediaSize> = ({ size, gap }) => (
     </Link>
     <Link href="https://discord.gg/fhsyaEJ5VZ" isExternal fontSize={size}>
       <Icon
+        color={color}
         as={FaDiscord}
         aria-label="Discord."
         _hover={{
