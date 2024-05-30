0dc98bfae1e887eaed456bb1900464f995bd402a
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-05-30 15:40:35 +0200

    Update the navigationData

---
+++ packages/frontend/src/modules/home/constants/navigationData.tsx
@@ -0,0 +1,24 @@
+import { FaGear, FaHouse, FaPaperPlane, FaUser } from "react-icons/fa6";
+
+export const navigationData = [
+  {
+    title: "home",
+    icon: FaHouse,
+    link: "#"
+  },
+  {
+    title: "messages",
+    icon: FaPaperPlane,
+    link: "#"
+  },
+  {
+    title: "profile",
+    icon: FaUser, // maybe replace with a user avatar
+    link: "#"
+  },
+  {
+    title: "settings",
+    icon: FaGear,
+    link: "#"
+  }
+];
