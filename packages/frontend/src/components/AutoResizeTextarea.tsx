f3c5c5e451268bcee9d2792858d366c0468fb3e6
Diego <diego.milla.26@gmail.com>
2024-05-31 17:24:57 +0200

    Bump auto resize library

--- packages/frontend/src/components/AutoResizeTextarea.tsx
+++ packages/frontend/src/components/AutoResizeTextarea.tsx
@@ -1,5 +1,5 @@
 import { Textarea, TextareaProps } from "@chakra-ui/react";
-import ResizeTextarea from "react-autosize-textarea";
+import ResizeTextarea from "react-textarea-autosize";
 import React from "react";
 
 export const AutoResizeTextarea = React.forwardRef<
