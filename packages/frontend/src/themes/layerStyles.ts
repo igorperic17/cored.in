35e427dfa4215179f5e1c52e4d679b0549d1c2db
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-06-04 10:31:14 +0200

    Edit the layerStyles export

--- packages/frontend/src/themes/layerStyles.ts
+++ packages/frontend/src/themes/layerStyles.ts
@@ -2,9 +2,9 @@
 import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme with new layer styles
-export const layerStyles = extendTheme({
+export const layerStyles = {
   cardBox: {
     bg: "background.700",
     borderRadius: "0.5em"
   }
-});
+};
