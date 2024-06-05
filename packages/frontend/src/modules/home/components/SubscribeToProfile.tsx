e9007b8acb3af1c77e16ce8e4f5fcb76d40e43b6
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-06-05 15:36:25 +0200

    Centrally align the text in the SubscribeToProfile section

--- packages/frontend/src/modules/home/components/SubscribeToProfile.tsx
+++ packages/frontend/src/modules/home/components/SubscribeToProfile.tsx
@@ -9,12 +9,14 @@ export const SubscribeToProfile = () => {
       pb="1.75em"
       spacing="3em"
       boxShadow="0 0 15px 0px #7AF9B3"
+      my={{ base: "1em", lg: "none" }}
     >
       <Text
         textStyle="lg"
         // fontSize="1.5em"
         color="text.100"
         // textTransform="uppercase"
+        textAlign="center"
       >
         Subscribe to this user's profile to see their private information and
         send messages.
