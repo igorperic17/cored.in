0ed3d9720cc1fa2472cb5a36d1da930c2d5ac216
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-06-05 19:35:54 +0200

    Refine the styling in the settings page

--- packages/frontend/src/modules/home/views/Settings.page.tsx
+++ packages/frontend/src/modules/home/views/Settings.page.tsx
@@ -1,43 +1,65 @@
 import { AutoResizeTextarea } from "@/components";
 import {
-  Box,
-  Button,
-  Flex,
   FormControl,
   FormHelperText,
   FormLabel,
   HStack,
   Heading,
   Input,
-  InputGroup,
-  InputLeftElement,
   Text,
   VStack
 } from "@chakra-ui/react";
-import { UserHeader } from "../components";
 
 const SettingsPage = () => {
   return (
-    <VStack spacing="1.5em" layerStyle="cardBox" p="1em" pb="4em" align="start">
+    <VStack spacing="2.5em" layerStyle="cardBox" p="1em" mb="4em" align="start">
       <Heading as="h1" fontFamily="body">
         Edit your information
       </Heading>
-      <Text as="span">Avatar</Text>
-      <FormControl>
-        <FormLabel>Add a link to a photo</FormLabel>
-        <Input type="text" />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Initials color</FormLabel>
-        {/* Limit to 2 letters */}
-        <Input type="color" h="40px" w="80px" p="0" border="none" />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Color of the profile header</FormLabel>
-        <Input type="color" h="40px" w="80px" p="0" border="none" />
-      </FormControl>
+      <VStack spacing="1.25em" align="start">
+        <Text as="span" textStyle="lg">
+          Avatar
+        </Text>
+        <FormControl>
+          <FormLabel>Add a link to a photo</FormLabel>
+          <Input
+            type="text"
+            border="1px solid #828178"
+            focusBorderColor="brand.500"
+          />
+          <FormHelperText color="text.400">
+            First two letters of your username will be shown in cases when there
+            is no image provided.
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Letters color</FormLabel>
+          {/* Limit to 2 letters */}
+          <Input
+            type="color"
+            h="40px"
+            w="80px"
+            p="0"
+            border="none"
+            value="#7AF9B3"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Color of the profile header</FormLabel>
+          <Input
+            type="color"
+            h="40px"
+            w="80px"
+            p="0"
+            border="none"
+            value="#7AF9B3"
+          />
+        </FormControl>
+      </VStack>
       {/* <FormControl>
-        <FormLabel>Username</FormLabel>
+        <FormLabel fontSize={{ base: "1.25rem", lg: "1.5rem" }}>
+          Username
+        </FormLabel>
         <InputGroup>
           <InputLeftElement
             pointerEvents="none"
@@ -59,11 +81,12 @@ const SettingsPage = () => {
         </FormHelperText>
       </FormControl> */}
       <FormControl>
-        <FormLabel>Bio</FormLabel>
+        <FormLabel fontSize={{ base: "1.25rem", lg: "1.5rem" }}>Bio</FormLabel>
         <AutoResizeTextarea
           placeholder="Tell about yourself"
           minH="80px"
           border="1px solid #828178"
+          focusBorderColor="brand.500"
         />
         <FormHelperText color="text.400">
           Maximum length: 250 characters
