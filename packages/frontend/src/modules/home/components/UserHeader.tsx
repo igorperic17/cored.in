d2519fc95e08fba322e690b42f811f4cbfd05e26
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-06-05 15:24:08 +0200

    Fix the UserHeader layout on mobile

--- packages/frontend/src/modules/home/components/UserHeader.tsx
+++ packages/frontend/src/modules/home/components/UserHeader.tsx
@@ -1,7 +1,9 @@
-import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Icon, Link, Text } from "@chakra-ui/react";
 import { useChain } from "@cosmos-kit/react";
 import { TESTNET_CHAIN_NAME } from "@coredin/shared";
 import { FaPen } from "react-icons/fa6";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { ROUTES } from "@/router/routes";
 
 export const UserHeader = () => {
   const chainContext = useChain(TESTNET_CHAIN_NAME);
@@ -11,41 +13,53 @@ export const UserHeader = () => {
   return (
     <Box layerStyle="cardBox" w="100%">
       <Box
-        h={{ base: "75px", lg: "125px" }}
+        h={{ base: "75px", sm: "90px", lg: "125px" }}
         w="100%"
         bg="brand.500"
         borderTopRadius="inherit"
       ></Box>
-      <Avatar
-        name="U N"
-        // src="https://bit.ly/sage-adebayo"
-        bg="background.600"
-        color="brand.500"
-        size={{ base: "lg", lg: "2xl" }}
-        mt="-1.25em"
-        ml="0.5em"
-        border="4px solid #1C1C1A"
-      />
-      <Flex direction="column" gap="1.5em" px="1.125em" pt="1.75em" pb="2.5em">
-        <Flex justify="space-between">
-          <Text as="span" color="text.100" textStyle="md">
-            @username
-            {/* {post.creatorWallet} */}
+      <Flex
+        justify="space-between"
+        mx="1em"
+        // border="1px solid red"
+        align="center"
+        mt={{ base: "-2.5em", sm: "-3em", lg: "-4em" }}
+      >
+        <Avatar
+          name="U N"
+          // src="https://bit.ly/sage-adebayo"
+          bg="background.600"
+          color="brand.500"
+          size={{ base: "lg", sm: "xl", lg: "2xl" }}
+          // ml="0.5em"
+          border="4px solid #1C1C1A"
+        />
+        <Link
+          as={ReactRouterLink}
+          to={ROUTES.SETTINGS.path}
+          aria-label="Edit profile."
+          variant="empty"
+          size="sm"
+          color="text.400"
+          mt="3.5em"
+          // mt={{ base: "-3rem", md: "-4.5rem" }}
+          _hover={{
+            textDecoration: "none",
+            color: "text.100"
+          }}
+        >
+          <Icon as={FaPen} size="0.875rem" />
+          <Text as="span" ml="0.375em">
+            Edit
           </Text>
-          <Button
-            aria-label="Edit profile."
-            variant="empty"
-            size="sm"
-            color="text.400"
-            leftIcon={<FaPen size="0.875rem" />}
-            mt={{ base: "-3rem", md: "-4.5rem" }}
-          >
-            <Text as="span" ml="0.375em">
-              Edit
-            </Text>
-          </Button>
-        </Flex>
-        <Text textStyle="sm">
+        </Link>
+      </Flex>
+      <Flex direction="column" gap="1.5em" px="1.125em" pt="1.75em" pb="2.5em">
+        <Text as="span" color="text.100" textStyle="md">
+          @username
+          {/* {post.creatorWallet} */}
+        </Text>
+        <Text textStyle="sm" wordBreak="break-word">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
           corrupti atque optio eaque voluptas architecto quibusdam!
         </Text>
