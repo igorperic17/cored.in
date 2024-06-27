e653b041b67be71100f4414c211714f8163d096a
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-06-27 15:22:11 +0200

    Add basic style to the CredentialsMain

--- packages/frontend/src/modules/home/components/CredentialsMain.tsx
+++ packages/frontend/src/modules/home/components/CredentialsMain.tsx
@@ -1,9 +1,51 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, HStack, Heading, VStack } from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa6";
 
 export const CredentialsMain = () => {
   return (
     <Box layerStyle="cardBox" p="1em" pb="1.5em" mb="4em">
-      <Heading as="h1">This is the CredentialsMain component</Heading>
+      <Heading as="h1" fontFamily="body" mb="2.5em">
+        Credentials
+      </Heading>
+      <VStack spacing="2em" align="start">
+        <HStack spacing="1em">
+          <Heading
+            as="h2"
+            fontFamily="body"
+            fontSize={{ base: "1.25rem", lg: "1.5rem" }}
+          >
+            My credentials
+          </Heading>
+          <FaArrowRight />
+        </HStack>
+
+        <HStack spacing="1em">
+          <Heading
+            as="h2"
+            fontFamily="body"
+            fontSize={{ base: "1.25rem", lg: "1.5rem" }}
+          >
+            Issued credentials
+          </Heading>
+          <FaArrowRight />
+        </HStack>
+
+        {/* Should this lead to a different page or render the requests here? */}
+        <HStack>
+          <Heading
+            as="h2"
+            fontFamily="body"
+            fontSize={{ base: "1.25rem", lg: "1.5rem" }}
+            color="text.700" // conditionally change the color
+          >
+            Incoming requests:
+          </Heading>
+        </HStack>
+      </VStack>
     </Box>
   );
 };
+
+// My credentials
+// Incoming requests
+// Issued credentials
