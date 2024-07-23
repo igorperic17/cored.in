f0d7ea8f834b27df4e4c76d31f15f13a652a07c7
Diego <diego.milla.26@gmail.com>
2024-07-23 18:06:41 +0200

    Implement read subscription methods for CoredinContractService

--- packages/backend/src/coreum/services/CoredinContractService.ts
+++ packages/backend/src/coreum/services/CoredinContractService.ts
@@ -6,4 +6,30 @@ export class CoredinContractService {
   async getWalletInfo(wallet: string) {
     return this.coredinQueryClient.getWalletDID({ wallet });
   }
+
+  async isWalletSubscribed(profileWallet: string, subscriberWallet: string) {
+    const profileInfo = await this.getWalletInfo(profileWallet);
+    const subscriberInfo = await this.getWalletInfo(subscriberWallet);
+    if (!profileInfo.did_info || !subscriberInfo.did_info) {
+      console.error(
+        "No DID info found for profile or subscriber: ",
+        profileWallet,
+        profileInfo,
+        subscriberWallet,
+        subscriberInfo
+      );
+      return false;
+    }
+    return this.coredinQueryClient.isSubscriber({
+      did: profileInfo.did_info.did,
+      subscriber: subscriberInfo.did_info.did
+    });
+  }
+
+  async isSubscribed(profileDid: string, subscriberDid: string) {
+    return this.coredinQueryClient.isSubscriber({
+      did: profileDid,
+      subscriber: subscriberDid
+    });
+  }
 }
