5669e1460f7894e3b92e93162fb0b4f6d8c55ae8
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-07-24 09:56:49 +0200

    Connect the SubscriptionModal to the SubscribeToProfile component

--- packages/frontend/src/modules/home/components/SubscribeToProfile.tsx
+++ packages/frontend/src/modules/home/components/SubscribeToProfile.tsx
@@ -1,6 +1,49 @@
-import { Button, Text, VStack } from "@chakra-ui/react";
+import { CoredinClientContext } from "@/contexts/CoredinClientContext";
+import { Button, Text, useDisclosure, VStack } from "@chakra-ui/react";
+import { useChain } from "@cosmos-kit/react";
+import { FC, useContext, useEffect, useState } from "react";
+import { DidInfo, TESTNET_CHAIN_NAME } from "@coredin/shared";
+import { SubscriptionModal } from ".";
 
-export const SubscribeToProfile = () => {
+type SubscribeToProfileProps = {
+  profileDid: DidInfo;
+};
+
+export const SubscribeToProfile: FC<SubscribeToProfileProps> = ({
+  profileDid
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const chainContext = useChain(TESTNET_CHAIN_NAME);
+  const coredinClient = useContext(CoredinClientContext);
+  const [price, setPrice] = useState<number>();
+
+  // const updateOnchainPrice = () => {
+  //   if (chainContext.address) {
+  //     console.log("getting onchain price");
+  //     coredinClient
+  //       ?.getSubscriptionPrice({ did: profileDid.did })
+  //       .then((registered_did: GetDIDResponse) => {
+  //         console.log(registered_did);
+  //         if (registered_did.did_info) {
+  //           setOnchainProfile(registered_did.did_info);
+  //         }
+  //         setIsLoadingContract(false);
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //         setIsLoadingContract(false);
+  //       });
+  //   } else {
+  //     setOnchainProfile(null);
+  //   }
+  // };
+
+  // useEffect(updateOnchainPrice, [
+  //   chainContext.address,
+  //   chainContext.isWalletConnected,
+  //   coredinClient
+  // ]);
+
   return (
     <VStack
       layerStyle="cardBox"
@@ -21,9 +64,16 @@ export const SubscribeToProfile = () => {
         Subscribe to this user's profile to see their private information and
         send messages.
       </Text>
-      <Button variant="primary" size="md" w="100%" alignSelf="end">
-        Subscribe
+      <Button
+        variant="primary"
+        size="sm"
+        w="100%"
+        alignSelf="end"
+        onClick={onOpen}
+      >
+        Subscribe for 4.45 CORE
       </Button>
+      <SubscriptionModal isOpen={isOpen} onClose={onClose} />
     </VStack>
   );
 };
