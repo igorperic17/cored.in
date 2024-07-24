8053fc345fa64fc08ad8fa85c89634f80779d86c
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-07-24 11:41:11 +0200

    Render both setting pages in Tabs in SettingsPage

--- packages/frontend/src/modules/home/views/Settings.page.tsx
+++ packages/frontend/src/modules/home/views/Settings.page.tsx
@@ -1,7 +1,37 @@
-import { Settings } from "../components";
+import {
+  Box,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  VisuallyHidden
+} from "@chakra-ui/react";
+import { ProfileSettings, SubscriptionSettings } from "../components";
 
 const SettingsPage = () => {
-  return <Settings />;
+  return (
+    <Box w="100%">
+      <VisuallyHidden>
+        <Heading as="h1">Settings</Heading>
+      </VisuallyHidden>
+      <Tabs isFitted size="md" variant="unstyled">
+        <TabList>
+          <Tab>Profile</Tab>
+          <Tab>Subscription</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <ProfileSettings />
+          </TabPanel>
+          <TabPanel>
+            <SubscriptionSettings />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 };
 
 export default SettingsPage;
