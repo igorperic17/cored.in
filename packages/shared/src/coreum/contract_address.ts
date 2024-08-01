e708b3cb4364b453b54caca5d5999ecca693236d
Diego <diego.milla.26@gmail.com>
2024-08-01 09:13:10 +0200

    Update deployed contract address

--- packages/shared/src/coreum/contract_address.ts
+++ packages/shared/src/coreum/contract_address.ts
@@ -1,1 +1,1 @@
-export const CONTRACT_ADDRESS="testcore136me3ws0w5atvjga0nmnrvxrtjwkdp34h85y6escq0zrv6vpyvdsm80988";
+export const CONTRACT_ADDRESS="testcore1zx8wysks3u0xyetk5rx4ecq4qtgepjvkz23vqrz549v35h9q792sqr2s46";
