a78d2c97660aa397fbaa5a30bc233d60ffaee458
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-12 12:10:14 +0200

    Add transparentBox style

--- packages/frontend/src/themes/layerStyles.ts
+++ packages/frontend/src/themes/layerStyles.ts
@@ -2,5 +2,13 @@ export const layerStyles = {
   cardBox: {
     bg: "background.700",
     borderRadius: "0.5em"
+  },
+  transparentBox: {
+    py: "1.5em",
+    px: "1em",
+    borderRadius: "1em",
+    border: "1px solid #141413",
+    boxShadow: "0px 4px 4px 0px #00000014",
+    backdropFilter: "blur(12px)"
   }
 };
