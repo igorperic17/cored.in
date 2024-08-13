cd2107dab1aff6b57515c5b02167ef07bbdb4e7a
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-13 14:54:41 +0200

    Change badgeTheme

--- packages/frontend/src/themes/badgeTheme.ts
+++ packages/frontend/src/themes/badgeTheme.ts
@@ -3,8 +3,8 @@ import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
 const verified = defineStyle({
   p: "0.25em 0.5em",
   border: "1px solid",
-  borderColor: "brand.500",
-  color: "brand.500",
+  borderColor: "brand.300",
+  color: "brand.300",
   fontFamily: "body",
   fontWeight: "semibold"
 });
