d1c7f816c2b1a36ed8732e5709a99d523f793a81
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-13 14:33:08 +0200

    Add background patter with opacity 35 to the LoginRoot

--- packages/frontend/src/modules/login/views/LoginRoot.tsx
+++ packages/frontend/src/modules/login/views/LoginRoot.tsx
@@ -2,6 +2,7 @@ import { Box, Flex } from "@chakra-ui/react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Disclaimer } from "@/components/Disclaimer";
 import { Header } from "@/components";
+import LandingBg35 from "@/assets/landing-bg-35.png";
 
 export const LoginRoot = () => {
   return (
@@ -9,10 +10,12 @@ export const LoginRoot = () => {
       direction="column"
       justify="start"
       minH="100dvh"
+      bgImage={LandingBg35}
+      bgPosition="center"
       // border="2px solid red"
     >
       <Header />
-      <Box as="main">
+      <Box as="main" mb="auto">
         <Outlet />
         <ScrollRestoration />
       </Box>
