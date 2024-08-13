58bd93ac467b3f71b511a060828e84af4cb0e5ed
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-13 10:37:39 +0200

    Add background pattern

--- packages/frontend/src/modules/landing/views/Root.tsx
+++ packages/frontend/src/modules/landing/views/Root.tsx
@@ -2,12 +2,18 @@ import { Box, Flex } from "@chakra-ui/react";
 import { CookiesBar } from "@/components";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Footer, Nav } from "../components";
+import LandingBg50 from "@/assets/landing-bg-50.png";
 
 export const Root = () => {
   return (
-    <Flex direction="column" justify="start">
+    <Flex
+      direction="column"
+      justify="start"
+      bgImage={LandingBg50}
+      bgPosition="center"
+    >
       <Nav />
-      <Box as="main">
+      <Box as="main" zIndex="0">
         <Outlet />
         <ScrollRestoration />
       </Box>
