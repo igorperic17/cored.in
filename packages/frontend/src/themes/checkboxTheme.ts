ec03dd4db8ab0e0edeb073f57c44dd55a2672630
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-21 16:10:49 +0200

    Edit checkboxTheme colors

--- packages/frontend/src/themes/checkboxTheme.ts
+++ packages/frontend/src/themes/checkboxTheme.ts
@@ -10,21 +10,21 @@ const baseStyle = definePartsStyle({
   },
   control: {
     borderRadius: "2px",
-    borderColor: "brand.100",
+    borderColor: "brand.900",
     borderWidth: "1px",
     p: 1,
     mr: "6px",
     _hover: {
-      borderColor: "brand.300",
+      borderColor: "brand.200",
       _checked: {
-        borderColor: "brand.100",
-        bg: "brand.100"
+        borderColor: "brand.900",
+        bg: "brand.900"
       }
     },
     _checked: {
-      bg: "brand.300",
-      borderColor: "brand.300",
-      color: "brand.900"
+      bg: "brand.200",
+      borderColor: "brand.200",
+      color: "brand.100"
     }
   }
 });
