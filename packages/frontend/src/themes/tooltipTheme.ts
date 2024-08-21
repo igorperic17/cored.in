9099ffa3010665f47d87511c984f43c450751483
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-21 17:07:55 +0200

    Add tooltipTheme

---
+++ packages/frontend/src/themes/tooltipTheme.ts
@@ -0,0 +1,9 @@
+import { defineStyleConfig } from "@chakra-ui/react";
+
+const baseStyle = {
+  borderRadius: "0.25em",
+  fontSize: "0.875rem",
+  fontWeight: "400"
+};
+
+export const tooltipTheme = defineStyleConfig({ baseStyle });
