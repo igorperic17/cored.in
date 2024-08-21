d4e1c48d08e5f88d3719d37fda149b84ae214c08
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-21 11:41:44 +0200

    Replace the background image

--- packages/frontend/src/modules/landing/views/Root.tsx
+++ packages/frontend/src/modules/landing/views/Root.tsx
@@ -2,14 +2,14 @@ import { Box, Flex } from "@chakra-ui/react";
 import { CookiesBar } from "@/components";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Footer, Nav } from "../components";
-import LandingBg35 from "@/assets/landing-bg-35.png";
+import LandingBgDark35 from "@/assets/landing-bg-dark-35.png";
 
 export const Root = () => {
   return (
     <Flex
       direction="column"
       justify="start"
-      bgImage={LandingBg35}
+      bgImage={LandingBgDark35}
       bgPosition="center"
     >
       <Nav />
