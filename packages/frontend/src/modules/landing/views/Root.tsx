d1e4928b5a07d5145c502ff563e21868d0bbaa35
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-08-22 15:23:59 +0200

    Use MainBackground in Root

--- packages/frontend/src/modules/landing/views/Root.tsx
+++ packages/frontend/src/modules/landing/views/Root.tsx
@@ -1,24 +1,21 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { CookiesBar } from "@/components";
+import { CookiesBar, MainBackground } from "@/components";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Footer, Nav } from "../components";
-import LandingBgDark35 from "@/assets/landing-bg-dark-35.png";
 
 export const Root = () => {
   return (
-    <Flex
-      direction="column"
-      justify="start"
-      bgImage={LandingBgDark35}
-      bgPosition="center"
-    >
-      <Nav />
-      <Box as="main" zIndex="0">
-        <Outlet />
-        <ScrollRestoration />
-      </Box>
-      <Footer />
-      <CookiesBar />
-    </Flex>
+    <Box maxH="100%" w="100%" bg="brand.100" position="fixed" overflow="auto">
+      <Flex direction="column" justify="start">
+        <Nav />
+        <Box as="main" zIndex="0">
+          <Outlet />
+          <ScrollRestoration />
+        </Box>
+        <Footer />
+        <CookiesBar />
+      </Flex>
+      <MainBackground />
+    </Box>
   );
 };
