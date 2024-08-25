1cdb022de51926d7246a9879318be1f5e1d4ed18
Diego <diego.milla.26@gmail.com>
2024-08-25 15:56:04 +0200

    Implement getSubscribers and getSubscriptions paginated ContractQueries

--- packages/frontend/src/queries/ContractQueries.ts
+++ packages/frontend/src/queries/ContractQueries.ts
@@ -1,10 +1,17 @@
 import { BaseServerStateKeys } from "@/constants";
 import { CoredinClient } from "@coredin/shared";
 
+// TODO: increase page size after pagination is implemented
+const PAGE_SIZE = 2;
+
 export const CONTRACT_QUERIES = {
   getWalletDid: (coredinClient: CoredinClient, wallet: string) => ({
     queryKey: [BaseServerStateKeys.CONTRACT_GET_WALLET_DID, wallet],
     queryFn: () => coredinClient.getWalletDID({ wallet })
+  }),
+  getInfoFromDid: (coredinClient: CoredinClient, did: string) => ({
+    queryKey: [BaseServerStateKeys.CONTRACT_GET_DID_INFO, did],
+    queryFn: () => coredinClient.getDID({ did })
   }),
   getSubscriptionPrice: (coredinClient: CoredinClient, did: string) => ({
     queryKey: [BaseServerStateKeys.CONTRACT_GET_SUBSCRIPTION_PRICE, did],
@@ -13,6 +20,56 @@ export const CONTRACT_QUERIES = {
   getSubscriptionDuration: (coredinClient: CoredinClient, did: string) => ({
     queryKey: [BaseServerStateKeys.CONTRACT_GET_SUBSCRIPTION_DURATION, did],
     queryFn: () => coredinClient.getSubscriptionDuration({ did })
+  }),
+  getSubscribers: (coredinClient: CoredinClient, wallet: string) => ({
+    queryKey: [
+      BaseServerStateKeys.CONTRACT_GET_SUBSCRIBERS,
+      wallet,
+      coredinClient.contractAddress
+    ],
+    queryFn: async ({ pageParam }: { pageParam: number }) => {
+      console.log(
+        "pageParam",
+        pageParam,
+        "wallet",
+        wallet,
+        "coredinClient",
+        coredinClient
+      );
+      console.log("PAGE_SIZE.toString()", PAGE_SIZE.toString());
+      const res = await coredinClient.getSubscribers({
+        wallet,
+        page: pageParam.toString(),
+        pageSize: PAGE_SIZE.toString()
+      });
+      console.log("res", res);
+      return res;
+    }
+  }),
+  getSubscriptions: (coredinClient: CoredinClient, wallet: string) => ({
+    queryKey: [
+      BaseServerStateKeys.CONTRACT_GET_SUBSCRIPTIONS,
+      wallet,
+      coredinClient.contractAddress
+    ],
+    queryFn: async ({ pageParam }: { pageParam: number }) => {
+      console.log(
+        "getSubscriptionspageParam",
+        pageParam,
+        "getSubscriptionswallet",
+        wallet,
+        "getSubscriptionscoredinClient",
+        coredinClient
+      );
+      console.log("getSubscriptionsPAGE_SIZE.toString()", PAGE_SIZE.toString());
+      const res = await coredinClient.getSubscriptions({
+        wallet,
+        page: pageParam.toString(),
+        pageSize: PAGE_SIZE.toString()
+      });
+      console.log("getSubscriptionsres", res);
+      return res;
+    }
   }),
   isSubscriber: (
     coredinClient: CoredinClient,
