d3565e69c4de86c9fa1833ff2a46c0804c27e207
Diego <diego.milla.26@gmail.com>
2024-08-29 18:26:12 +0200

    Implement getAllSubscriptions in backend contract service

--- packages/backend/src/coreum/services/CoredinContractService.ts
+++ packages/backend/src/coreum/services/CoredinContractService.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CoredinQueryClient } from "@coredin/shared";
+
+const PAGE_SIZE = 1;
 
 export class CoredinContractService {
   constructor(private readonly coredinQueryClient: CoredinQueryClient) {}
@@ -19,10 +22,53 @@ export class CoredinContractService {
       );
       return false;
     }
-    return this.coredinQueryClient.isSubscriber({
+
+    const isSubscribed = await this.coredinQueryClient.isSubscriber({
       targetDid: profileInfo.did_info.did,
       subscriberWallet
     });
+
+    return isSubscribed;
+  }
+
+  async getAllSubscriptions(profileWallet: string) {
+    const allSubscriptions = [];
+    let page = 0;
+    let continueFetching = true;
+    while (continueFetching) {
+      const subscriptions = await this.getSubscriptions(profileWallet, page);
+      if (
+        !subscriptions.subscribers ||
+        subscriptions.subscribers.length === 0
+      ) {
+        continueFetching = false;
+        break;
+      }
+      allSubscriptions.push(...subscriptions.subscribers);
+      continueFetching = subscriptions.subscribers.length === PAGE_SIZE;
+      page++;
+    }
+
+    return allSubscriptions;
+  }
+
+  async getSubscriptions(
+    requesterWallet: string,
+    page: number,
+    pageSize: number = PAGE_SIZE
+  ) {
+    try {
+      const res = await this.coredinQueryClient.getSubscriptions({
+        wallet: requesterWallet,
+        page: page.toString(),
+        pageSize: pageSize.toString()
+      });
+      console.log("getSubscriptionsres", res);
+      return res;
+    } catch (e: any) {
+      console.error("Error fetching subscriptions: ", e, requesterWallet);
+      return { subscribers: [] };
+    }
   }
 
   // async isSubscribed(profileDid: string, subscriberDid: string) {
