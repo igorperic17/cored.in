661958b4a25d943a54c0bb4c5f2d9721b01e5153
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-09-11 14:12:58 +0200

    Change the overflow of the AutoResizeTextarea

--- packages/frontend/src/components/AutoResizeTextarea.tsx
+++ packages/frontend/src/components/AutoResizeTextarea.tsx
@@ -11,12 +11,13 @@ export const AutoResizeTextarea = React.forwardRef<
     <Textarea
       as={ResizeTextarea}
       minH="60px"
-      overflow="hidden"
+      // overflow="hidden"
       w="100%"
       resize="none"
       ref={ref}
       minRows={1}
       fontSize={{ base: "0.875rem", lg: "1rem" }}
+      overflow="auto"
       {...formElementBorderStyles}
       _placeholder={{
         color: "text.700"
