f724e560fd1ca29a946363e04163e29ec5dc38db
Diego <diego.milla.26@gmail.com>
2024-10-04 10:38:32 +0200

    Add warning message and link to faucet on registration page if not enough CORE tokens to register

--- packages/frontend/src/modules/login/components/NotRegisteredProfile.tsx
+++ packages/frontend/src/modules/login/components/NotRegisteredProfile.tsx
@@ -1,3 +1,5 @@
+import { useCustomToast } from "@/hooks";
+import { CopyIcon } from "@chakra-ui/icons";
 import {
   Button,
   FormControl,
@@ -5,11 +7,14 @@ import {
   HStack,
   Heading,
   Input,
+  Link,
   Text,
   VStack,
   VisuallyHidden
 } from "@chakra-ui/react";
-import { ChangeEvent, FC } from "react";
+import { TESTNET_CHAIN_NAME, TESTNET_STAKING_DENOM } from "@coredin/shared";
+import { useChain } from "@cosmos-kit/react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 
 interface ProfileRegistrationProps {
   did: string;
@@ -26,6 +31,28 @@ export const NotRegisteredProfile: FC<ProfileRegistrationProps> = ({
   registerProfile,
   isRegistering
 }) => {
+  const chainContext = useChain(TESTNET_CHAIN_NAME);
+  const [balance, setBalance] = useState(0);
+  const { successToast } = useCustomToast();
+
+  const getBalance = async () => {
+    if (!chainContext.address) {
+      setBalance(0);
+      return;
+    }
+
+    const client = await chainContext.getCosmWasmClient();
+    const coin = await client.getBalance(
+      chainContext.address,
+      TESTNET_STAKING_DENOM
+    );
+
+    // TODO - REVIEW DECIMALS AFTER TESTNET, utestcore has 6 decimals!
+    setBalance(parseInt(coin.amount) / 1000000);
+  };
+  useEffect(() => {
+    getBalance();
+  }, [chainContext.address]);
   return (
     <HStack
       w="100%"
@@ -73,9 +100,44 @@ export const NotRegisteredProfile: FC<ProfileRegistrationProps> = ({
           <Text my="1em" color="text.300">
             At least 3 characters required, only letters and numbers allowed.
           </Text>
+          {balance < 1 && (
+            <VStack alignItems="center" gap="4px">
+              <Text my="1em" color="brand.400">
+                You're running low on CORE tokens, please top-up before
+                registering! Connected wallet:
+              </Text>
+              {chainContext.address && (
+                <Button
+                  variant="empty"
+                  size="sm"
+                  color="text.700"
+                  alignSelf="start"
+                  rightIcon={<CopyIcon ml="0.5em" />}
+                  aria-label="Copy connected wallet."
+                  mt="-1em"
+                  w="100%"
+                  onClick={() => {
+                    navigator.clipboard.writeText(chainContext.address!);
+                    successToast("Wallet copied to clipboard");
+                  }}
+                >
+                  {chainContext.address}
+                </Button>
+              )}
+              <Link
+                href={"https://docs.coreum.dev/docs/tools/faucet"}
+                isExternal
+                aria-label={`Link to coreum faucet.`}
+                color="brand.500"
+                // border="1px solid red"
+              >
+                Go to the Faucet
+              </Link>
+            </VStack>
+          )}
           <Button
             mt="2em"
-            isDisabled={usernameInput.length < 3}
+            isDisabled={usernameInput.length < 3 || balance < 1}
             onClick={registerProfile}
             size="md"
             variant="primary"
