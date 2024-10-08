686f58bf805e720a3856263e821c13f7a5e79e3b
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-10-08 19:45:34 +0400

    Change the tooltip style

--- packages/frontend/src/themes/tooltipTheme.ts
+++ packages/frontend/src/themes/tooltipTheme.ts
@@ -1,11 +1,13 @@
 import { defineStyleConfig } from "@chakra-ui/react";
 
 const baseStyle = {
-  borderRadius: "0.25em",
+  borderRadius: "1.125em",
   fontSize: "0.875rem",
   fontWeight: "400",
   color: "brand.100",
-  textAlign: "center"
+  textAlign: "center",
+  mt: "0.5em",
+  bg: "brand.900"
 };
 
 export const tooltipTheme = defineStyleConfig({ baseStyle });
