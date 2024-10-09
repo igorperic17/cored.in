19868f5764d306668bf876c01bf4931851e31022
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-10-09 14:34:26 +0400

    Delete NewPostContent

--- packages/frontend/src/modules/home/components/post/components/index.ts
+++ packages/frontend/src/modules/home/components/post/components/index.ts
@@ -1,5 +1,4 @@
 export { ActionBar } from "./ActionBar";
 export { Content } from "./Content";
-export { NewPostContent } from "./NewPostContent";
 export { NewMessageContent } from "./NewMessageContent";
 export { TippingModal } from "./TippingModal";
