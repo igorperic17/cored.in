aa323cf4db5ec2e68876ba66864362d54cbdb5f0
Diego <diego.milla.26@gmail.com>
2024-10-09 19:01:01 +0200

    Add confetti on tip success

--- packages/frontend/src/modules/home/components/post/components/TippingModal.tsx
+++ packages/frontend/src/modules/home/components/post/components/TippingModal.tsx
@@ -1,3 +1,4 @@
+import confetti from "canvas-confetti";
 import { useCustomToast } from "@/hooks";
 import { formElementBorderStyles } from "@/themes";
 import {
@@ -43,6 +44,7 @@ export const TippingModal: FC<TippingModalProps> = ({
       if (success) {
         setIsTipModalOpen(false);
         successToast("Tip sent successfully");
+        sendConfetti();
       } else {
         errorToast("Failed to send tip");
       }
@@ -52,6 +54,55 @@ export const TippingModal: FC<TippingModalProps> = ({
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const sendConfetti = () => {
+    const scalar = 2;
+    const triangle = confetti.shapeFromPath({
+      path: "M0 10 L5 0 L10 10z"
+    });
+    const square = confetti.shapeFromPath({
+      path: "M0 0 L10 0 L10 10 L0 10 Z"
+    });
+    const coin = confetti.shapeFromPath({
+      path: "M5 0 A5 5 0 1 0 5 10 A5 5 0 1 0 5 0 Z"
+    });
+    const tree = confetti.shapeFromPath({
+      path: "M5 0 L10 10 L0 10 Z"
+    });
+
+    const defaults = {
+      spread: 360,
+      ticks: 180,
+      gravity: 0,
+      decay: 0.96,
+      startVelocity: 20,
+      shapes: [triangle, square, coin, tree],
+      scalar
+    };
+
+    const shoot = () => {
+      confetti({
+        ...defaults,
+        particleCount: 30
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 5
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 15,
+        scalar: scalar / 2,
+        shapes: ["circle"]
+      });
+    };
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
   };
 
   return (
