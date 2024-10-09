4a14a8ad014bde7a941c87e069ab611a78524806
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-10-09 14:29:13 +0400

    Move the content of NewReplyContent to NewPost

--- packages/frontend/src/modules/home/components/NewPost.tsx
+++ packages/frontend/src/modules/home/components/NewPost.tsx
@@ -11,6 +11,9 @@ import { useState } from "react";
 import { NewReplyContent } from "./post/components";
 import { useChain } from "@cosmos-kit/react";
 import { USER_QUERIES } from "@/queries";
+import { Avatar, Button, Flex, Icon } from "@chakra-ui/react";
+import { AutoResizeTextarea } from "@/components";
+import { FaArrowUp } from "react-icons/fa6";
 
 export type NewPostProps = {
   replyToPostId?: number;
@@ -47,27 +50,44 @@ export const NewPost: React.FC<NewPostProps> = ({ replyToPostId }) => {
   return (
     <>
       {userProfile && replyToPostId && (
-        <NewReplyContent
-          postContent={postContent}
-          setPostContent={setPostContent}
-          handlePost={handlePost}
-          isLoading={isPending}
-          visibility={visibility}
-          setVisibility={setVisibility}
-          userProfile={userProfile}
-        />
+        <Flex
+          direction="row"
+          align="center"
+          gap="1.125em"
+          w="100%"
+          h="max-content"
+          // outline="1px solid red"
+          p="1.125em"
+        >
+          <Avatar
+            name={userProfile.username}
+            src={userProfile.avatarUrl}
+            size="sm"
+            bg="brand.100"
+            color={userProfile.avatarFallbackColor || "brand.500"}
+            border={userProfile.avatarUrl || "1px solid #b0b0b0"}
+          />
+          <AutoResizeTextarea
+            value={postContent}
+            onChange={(e) => setPostContent(e.target.value)}
+            variant="unstyled"
+            p="0.5em"
+            minH="unset"
+            placeholder="Add your comment"
+          />
+          <Button
+            variant="primary"
+            size="sm"
+            onClick={handlePost}
+            isLoading={isPending}
+            isDisabled={!postContent}
+            aria-label="post"
+            px="0"
+          >
+            <Icon as={FaArrowUp} />
+          </Button>
+        </Flex>
       )}
-      {/* // ) : (
-        //   <NewPostContent
-        //     postContent={postContent}
-        //     setPostContent={setPostContent}
-        //     handlePost={handlePost}
-        //     isLoading={isPending}
-        //     visibility={visibility}
-        //     setVisibility={setVisibility}
-        //     userProfile={userProfile}
-        //   />
-        // ))} */}
     </>
   );
 };
--- packages/frontend/src/modules/home/components/post/components/NewReplyContent.tsx
+++ packages/frontend/src/modules/home/components/post/components/NewReplyContent.tsx
@@ -12,42 +12,6 @@ export const NewReplyContent: FC<NewContentProps> = ({
   userProfile
 }) => {
   return (
-    <Flex
-      direction="row"
-      align="center"
-      gap="1.125em"
-      w="100%"
-      h="max-content"
-      // outline="1px solid red"
-      p="1.125em"
-    >
-      <Avatar
-        name={userProfile.username}
-        src={userProfile.avatarUrl}
-        size="sm"
-        bg="brand.100"
-        color={userProfile.avatarFallbackColor || "brand.500"}
-        border={userProfile.avatarUrl || "1px solid #b0b0b0"}
-      />
-      <AutoResizeTextarea
-        value={postContent}
-        onChange={(e) => setPostContent(e.target.value)}
-        variant="unstyled"
-        p="0.5em"
-        minH="unset"
-        placeholder="Add your comment"
-      />
-      <Button
-        variant="primary"
-        size="sm"
-        onClick={handlePost}
-        isLoading={isLoading}
-        isDisabled={!postContent}
-        aria-label="post"
-        px="0"
-      >
-        <Icon as={FaArrowUp} />
-      </Button>
-    </Flex>
+
   );
 };
