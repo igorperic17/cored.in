e6afcb5ef49d4c2e134f3b5d2814c08df3cd5445
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-10-10 16:30:12 +0400

    Incorporate logo into the confetti

--- packages/frontend/src/modules/home/components/post/components/TippingModal.tsx
+++ packages/frontend/src/modules/home/components/post/components/TippingModal.tsx
@@ -62,17 +62,8 @@ export const TippingModal: FC<TippingModalProps> = ({
 
   const sendConfetti = () => {
     const scalar = 2;
-    const triangle = confetti.shapeFromPath({
-      path: "M0 10 L5 0 L10 10z"
-    });
-    const square = confetti.shapeFromPath({
-      path: "M0 0 L10 0 L10 10 L0 10 Z"
-    });
-    const coin = confetti.shapeFromPath({
-      path: "M5 0 A5 5 0 1 0 5 10 A5 5 0 1 0 5 0 Z"
-    });
-    const tree = confetti.shapeFromPath({
-      path: "M5 0 L10 10 L0 10 Z"
+    const coredinLogo = confetti.shapeFromPath({
+      path: "M408.643 437.066C308.942 476.966 231.48 545.206 186.613 623.445C160.176 669.56 179.393 728.422 227.628 750.727C419.909 839.629 678.012 736.295 755.957 539.281C775.505 489.851 748.847 433.958 697.906 418.821C611.485 393.119 508.345 397.166 408.643 437.066Z M245.313 28.6195C245.037 28.73 244.761 28.8405 244.471 28.951C193.943 49.3084 172.863 109.801 199.576 157.283C287.171 312.988 518.299 414.609 692.977 374.806C746.556 362.597 776.941 305.779 757.628 254.305C683.493 56.6558 449.328 -56.4563 245.313 28.6195Z M253.775 293.901C236.559 250.839 208.479 215.317 173.842 189.117C128.16 154.562 61.9085 170.638 37.4317 222.43C-10.0448 322.918 -13.0957 446.43 32.3375 550.676C55.6132 604.097 123.066 622.065 169.548 586.943C257.35 520.609 297.454 400.44 253.775 293.901Z"
     });
 
     const defaults = {
@@ -81,26 +72,27 @@ export const TippingModal: FC<TippingModalProps> = ({
       gravity: 0,
       decay: 0.96,
       startVelocity: 20,
-      shapes: [triangle, square, coin, tree],
+      shapes: [coredinLogo],
+      colors: ["#FBB030", "#FF550F", "#7F02FE"],
       scalar
     };
 
     const shoot = () => {
       confetti({
         ...defaults,
-        particleCount: 30
+        particleCount: 30,
+        shapes: [coredinLogo]
       });
-
       confetti({
         ...defaults,
-        particleCount: 5
+        particleCount: 5,
+        shapes: [coredinLogo]
       });
-
       confetti({
         ...defaults,
         particleCount: 15,
         scalar: scalar / 2,
-        shapes: ["circle"]
+        shapes: [coredinLogo]
       });
     };
 
@@ -159,7 +151,7 @@ export const TippingModal: FC<TippingModalProps> = ({
                 </Tr>
                 <Tr borderBottom="1px solid black">
                   <Td pl="0" pt="0.5em" pb="1em" w="70%">
-            Platform commission 5%:
+                    Platform commission 5%:
                   </Td>
                   <Td px="0" pt="0.5em" pb="1em" textAlign="right">
                     {tipAmount ? commissionAmount.toFixed(2) : "0.00"} CORE
