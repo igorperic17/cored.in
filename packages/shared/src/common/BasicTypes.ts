acc9bb2009e104c0aaea2b87ab21a277cc60c676
Diego <diego.milla.26@gmail.com>
2024-11-04 09:27:21 +0100

    Fix PhotoURL regex not allowing capital letters in photo URL

--- packages/shared/src/common/BasicTypes.ts
+++ packages/shared/src/common/BasicTypes.ts
@@ -9,6 +9,6 @@ export type DateString = string &
   tags.Pattern<"((?:19|20)\\d\\d)-(0?[1-9]|1[012])-([12][0-9]|3[01]|0?[1-9])">;
 
 export type PhotoUrl = string &
-  tags.Pattern<`(http)?s?:([a-z\-_0-9\/\:\.]*\.(png|jpg|jpeg|gif|png|svg))`>;
+  tags.Pattern<`(http)?s?:([a-zA-Z\-_0-9\/\:\.]*\.(png|jpg|jpeg|gif|png|svg))`>;
 
 export type HexColor = string & tags.Pattern<`^#(?:[0-9a-fA-F]{3}){1,2}$`>;
